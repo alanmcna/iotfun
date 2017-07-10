9e924fac2801b53ed61912d7003014cbf11c96bb
Alan McNatty <alan@catalyst.net.nz>
2017-07-10 12:01:48 +1200

    Updated to only insert on change

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # iotfun
 Look at a pi based startdard IoT build
+
+- Get C3 
+-- cd www
+-- wget https://github.com/c3js/c3/archive/v0.4.14.zip
+-- unzip v0.4.14.zip
+-- ln -s c3-0.4.14 c3
+-- rm v0.4.14.zip
+
+- get D3 (last v3 version to work with C3)
+-- cd www
+-- wget https://github.com/d3/d3/releases/download/v3.5.17/d3.zip
+-- unzip d3.zip
--- sensor_uvindex.py
+++ sensor_uvindex.py
@@ -1,7 +1,11 @@
-# Modified simple example of reading the MCP3008 analog input channels and printing
-# them all out.
-# Author: Alan McNatty (original by Tony DiCola)
+# Simple example of reading the MCP3008 analog input channels and printing them all out.
+#
+# Requires Ahttps://github.com/adafruit/Adafruit_Python_MCP3008
+#
+# Author: Alan McNatty - based very very heavily on example script for the MCP3008 
+#                        written by Tony DiCola
 # License: Public Domain
+
 import time
 import psycopg2
 
@@ -24,18 +28,21 @@ mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 def mapfloat(x, in_min, in_max, out_min, out_max):
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 
-#print('Reading MCP3008 values, press Ctrl-C to quit...')
+#print('INFO: Reading MCP3008 values, press Ctrl-C to quit...')
 # Print nice channel column headers.
 conn = psycopg2.connect("dbname=sensor user=pi host=localhost")
 cur = conn.cursor()
 
 # Main program loop.
+last = 0.0
 while True:
     outputVoltage = 3.3 * float(mcp.read_adc(0)) / 1023
     uvIntensity = mapfloat(outputVoltage, 0.99, 2.9, 0.0, 15.0);
-    #print "{:f}".format(uvIntensity)
-    cur.execute("INSERT INTO reading (name, value) VALUES (%s, %s)", ("UV Index", str(uvIntensity)))
-    conn.commit()
+    if ( uvIntensity != last ):
+        print "INFO: UV Index = {:f}".format(uvIntensity)
+        cur.execute("INSERT INTO reading (name, value) VALUES (%s, %s)", ("UV Index", str(uvIntensity)))
+        conn.commit()
+        last = uvIntensity
     time.sleep(5)
 
 cur.close()
